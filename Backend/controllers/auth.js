a308b808d18a294ceee01c00f4298af52a03e14d
iMagnus506 <imagnus506@gmail.com>
2024-11-29 19:09:23 +0530

    Working on Backend

---
+++ Backend/controllers/auth.js
@@ -0,0 +1,30 @@
+const cryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const userSchema = require('../models/User');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Register User
+
+const registerUser = async(req, res) => {
+    const newUser = User({
+        name: req.body.name,
+        email: req.body.email,
+        password: cryptoJs.AES.encrypt(
+            req.body.password,
+            process.env.PASS
+        ).toString()
+    });
+    try{
+        const user = await newUser.save();
+        res.status(201).json(user);
+    }catch(error){
+        res.status(500).json(error);
+    }
+}
+
+// Login User
+
+// const loginUser = () => {
+
+// }
