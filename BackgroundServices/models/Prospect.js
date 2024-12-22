72bcb6a549a4f9ca56e2553ab4819158c3df618a
iMagnus506 <imagnus506@gmail.com>
2024-12-22 10:34:09 +0530

    started working on backgroundservices

---
+++ BackgroundServices/models/Prospect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const prospectSchema = mongoose.Schema({
+    name: {type: String, require: true},
+    email: {type: String, require: true},
+    address: {type: String},
+    tel: {type: String},
+    bloodgroup: {type: Number},
+    date: {type: String},
+    diseases: {type: String},
+    age: {type: String},
+    bloodpressure: {type: Number},
+    status: {type: Number, default: 0}
+}, {
+    timestamp : true
+})
+
+module.exports = mongoose.model('Prospect', prospectSchema);
